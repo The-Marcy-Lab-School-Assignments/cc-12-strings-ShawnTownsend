c0d03e9cefeba3f2069d32372ff9ca244fa39719
ShawnTownsend <stitans28@gmail.com>
2021-11-03 14:48:51 +0000

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//write your code here
+//Algorithm
+//Step 1: Declare a function named reverseString that takes a parameter.
+//Step 2: Declare a variable for the new string.
+//Step 3: Create condition for string argument that reverses string
+//Step 4: Return string
+
+function reverseString(str) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+reverseString("Shawn");
